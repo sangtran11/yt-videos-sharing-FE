86c8854ac8f37e36b480cee981ba65e0a42f0bc3
SangTran <sang.tran@adurolife.com>
2022-05-08 22:59:00 +0700

    change api base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://sharing-movie-be.herokuapp.com/" });
 
 export const signIn = (formData) => API.post("/user/signin", formData);
 export const signUp = (formData) => API.post("/user/signup", formData);
